5333d8a089178e575a9915d685dda86a06481e74
Cheri <mightypillow@tutanota.com>
2021-01-19 21:21:58 -0500

    Create operate function

--- calculator.js
+++ calculator.js
@@ -19,3 +19,16 @@ function divide(x, y) {
     console.error(e.message);
   }
 }
+
+function operate(op, x, y) {
+  switch(op) {
+    case "+":
+      return add(x, y);
+    case "-":
+      return subtract(x, y);
+    case "*":
+      return multiply(x, y);
+    case "/":
+      return divide(x, y);
+  }
+}
