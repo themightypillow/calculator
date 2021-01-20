226ba7a892ce5804a0b2986ec7f2f3cd9102c20d
Cheri <mightypillow@tutanota.com>
2021-01-20 16:50:15 -0500

    Result from expression carries over

--- calculator.js
+++ calculator.js
@@ -43,6 +43,7 @@ function operate(op, x, y) {
 
 function evaluateExpression() {
   calculator.second = Number(calculator.display);
+  console.log(calculator);
   try {
     let result = operate(calculator.operation, calculator.first, calculator.second);
     const decimalPlaces = result.toString().split(".").pop().length;
@@ -60,6 +61,7 @@ function evaluateExpression() {
 
 function addNumberEventListeners() {
   numberBtns.forEach(number => number.addEventListener("click", event => {
+    if(!("operation" in calculator)) delete calculator.first;
     calculator.display += event.target.textContent;
     displayDiv.textContent = calculator.display;
     console.log(calculator);
@@ -68,14 +70,16 @@ function addNumberEventListeners() {
 
 function addOpEventListeners() {
   opBtns.forEach(opBtn => opBtn.addEventListener("click", event => {
-    if(calculator.display) {
-      if("first" in calculator) {
-        evaluateExpression();
-      } 
-      else {
-        calculator.first = Number(calculator.display);
-      }
+    if("first" in calculator ) {
       calculator.operation = event.target.dataset.op;
+      if(calculator.display) {
+        evaluateExpression();
+      }
+      console.log(calculator);
+    }
+    else if(calculator.display) {
+      calculator.operation = event.target.dataset.op;
+      calculator.first = Number(calculator.display);
       calculator.display = "";
       console.log(calculator);
     }
@@ -86,7 +90,6 @@ function addEqualsEventListener() {
   equalsBtn.addEventListener("click", event => {
     if("first" in calculator && "operation" in calculator) {
       evaluateExpression();
-      delete calculator.first;
       calculator.display = "";
       console.log(calculator);
     }
